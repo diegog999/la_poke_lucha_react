ca5f2ae62c15cfa5663cdbda7b84c4cff06e4fb5
diegog999 <42815611+diegog999@users.noreply.github.com>
2021-02-09 18:58:42 +0100

    react app installation & netlify deployment & first routes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Switch, Route } from "react-router-dom";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [pokemon, setPokemon] = useState();
+  useEffect(() => {
+    const requestPokemon = async () => {
+      try {
+        const request = await axios.get(
+          "https://la-poke-lucha.herokuapp.com/pokemon"
+        );
+        setPokemon(request);
+      } catch (e) {
+        console.error(Error);
+      }
+    };
+    requestPokemon();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Switch>
+        <Route path="/pokemon/:id/:info"></Route>
+        <Route path="/pokemon/:id/">This is the id route</Route>
+        <Route path="/">{JSON.stringify(pokemon)}</Route>
+      </Switch>
     </div>
   );
 }
