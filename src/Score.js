370c0d885f7158d2612af31349ad4f8752a7bc5b
g-weiher <74683223+g-weiher@users.noreply.github.com>
2021-02-19 17:39:52 +0100

    remove some states

--- src/Score.js
+++ src/Score.js
@@ -1,17 +1,30 @@
 //--Material UI components
-import { AppBar, Box, Container, Typography, Divider } from "@material-ui/core";
+import { AppBar, Box, Container, Typography } from "@material-ui/core";
+import { Link } from "react-router-dom";
 //--Material UI style
 import { makeStyles } from "@material-ui/core/styles";
-import ScoreCard from "./Components/ScoreCard"
+
 import axios from "axios";
 import { useState, useEffect } from "react";
 
+import ScoreCard from "./Components/ScoreCard";
+
 const useStyles = makeStyles((theme) => ({
   //offset for fixed AppBar
-  offset: {height: "170px"},
+  offset: { height: "150px" },
   scoreCard: { marginBottom: "1rem", padding: "1rem" },
   heading: { marginBottom: "2rem" },
   pokeImage: { maxWidth: "200px" },
+  list: { textAlign: "center" },
+  appbar: {
+    padding: "1rem",
+    height: "150px",
+    backgroundColor: "black",
+  },
+  noDecoration: {
+    textDecoration: "none",
+    color: "inherit",
+  },
 }));
 
 const Score = () => {
@@ -33,27 +46,48 @@ const Score = () => {
 
   return (
     <>
-      <AppBar position="fixed">
+      {/* Top Bar */}
+      <AppBar position="fixed" className={classes.appbar}>
         <Box display="flex" justifyContent="space-between">
-          <Typography>choose your fighter</Typography>
-          <Typography variant="h1">la poke lucha</Typography>
-        </Box>
-        <Box display="flex" justifyContent="flex-end" alignItems="center">
-          <Typography>fight scores</Typography>
+          <Typography
+            variant="h4"
+            style={{ fontFamily: "'Press Start 2P', cursive" }}
+          >
+            <Link to="/" className={classes.noDecoration}>
+              choose <br></br>your fighter
+            </Link>
+          </Typography>
+
+          {/*<Box className={classes.results}>
+            <Typography variant="h4">Score</Typography>
+            <Box>
+              <Typography>Winner</Typography>
+              <Typography>{luchador1.name}</Typography>
+              <Typography>280</Typography>
+            </Box>
+            <Box>
+              <Typography>Loser</Typography>
+              <Typography>{luchador2.name}</Typography>
+              <Typography>70</Typography>
+            </Box>
+  </Box>*/}
+
+          <Typography variant="h4" style={{ fontFamily: "'Bangers', cursive" }}>
+            la poke lucha
+          </Typography>
         </Box>
       </AppBar>
 
       <div className={classes.offset} />
       <div>
-        <Container maxWidth="md">
+        <Container maxWidth="md" className={classes.list}>
           <Typography variant="h2" className={classes.heading}>
             Scores
           </Typography>
           {games.map((game) => (
             <>
-                {/*<Divider></Divider>*/}
-                <ScoreCard game={game}></ScoreCard>
-                
+              {/*<Divider></Divider>*/}
+              <ScoreCard game={game}></ScoreCard>
             </>
           ))}
         </Container>
